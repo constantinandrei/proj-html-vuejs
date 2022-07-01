16ce137d0cf3f51a811c7e042f2cb77df369437e
constantinandrei <andrei.todirascu@gmail.com>
2022-07-01 22:07:35 +0200

    main layout complete

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,37 @@
 <template>
-    <div>
-
-    </div>
+    <footer class="py-5 bg-primary">
+        <div class="container">
+            <div class="row row-cols-4 align-items-center">
+                <div class="col">
+                    <img class="mb-2" src="img/logo-sidearea-1.png" alt="">
+                    <div class="fs-7">Let's Get Creative</div>
+                </div>
+                <div class="col">
+                    <div class="fs-7 mb-2">maree.qode@gmail.com</div>
+                    <div class="fs-7">+44645 321 789</div>
+                </div>
+                <div class="col">
+                    <div class="fs-7 mb-2">Avenue d'Auderghem 10</div>
+                    <div class="fs-7">1040 Brussels, Belgium</div>
+                </div>
+                <div class="col">
+                    <div class="fs-7 mb-2">Stay in touch with us</div>
+                    <div class="d-flex w-75">
+                        <div class="col">
+                            <font-awesome-icon icon="fa-brands fa-twitter" />
+                        </div>
+                        <div class="col">
+                            <font-awesome-icon icon="fa-brands fa-pinterest-p" />
+                        </div>
+                        <div class="col">
+                            <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                        </div>
+                        <div class="col"><font-awesome-icon icon="fa-brands fa-linkedin-in" /></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -9,3 +39,9 @@
         name: 'TheFooter'
     }
 </script>
+
+<style lang="scss" scoped>
+    .fs-7 {
+        font-size: 0.8rem;
+    }
+</style>
