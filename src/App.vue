5720e6f489793c501a64d4ff9b590a25b77f66df
constantinandrei <andrei.todirascu@gmail.com>
2022-07-01 12:55:21 +0200

    navbar main layout

--- src/App.vue
+++ src/App.vue
@@ -29,5 +29,6 @@ export default {
 </script>
 
 <style lang="scss">
+  @import './assets/scss/variables.scss';
   @import './assets/scss/main.scss'
 </style>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,15 +1,27 @@
 <template>
-    <nav class="navbar">
+    <nav class="navbar py-2 px-3">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="img/logo-sidearea-1.png" alt="">
             </a>
-            <div class="d-flex">
-                <ul class="navbar-nav ms-auto flex-row gap-3">
+
+            <ul class="navbar-nav ms-auto flex-row gap-5 me-5">
                 <li v-for="link in navLinks" :key="link" class="nav-item">
                     <a class="nav-link text-black" href="#">{{link}}</a>
                 </li>
             </ul>
+            <div class="img-container">
+                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23">
+                    <circle cx="3" cy="20.1" r="2" />
+                    <circle cx="11.7" cy="20.1" r="2" />
+                    <circle cx="20.2" cy="20.1" r="2" />
+                    <circle cx="3" cy="2.9" r="2" />
+                    <circle cx="11.7" cy="2.9" r="2" />
+                    <circle cx="20.2" cy="2.9" r="2" />
+                    <circle cx="3" cy="11.4" r="2" />
+                    <circle cx="11.7" cy="11.4" r="2" />
+                    <circle cx="20.2" cy="11.4" r="2" />
+                </svg>
             </div>
         </div>
     </nav>
@@ -32,6 +44,9 @@
         z-index: 5;
         position: sticky;
         top: 0;
+        font-size: .7rem;
+        font-weight: bold;
+        letter-spacing: 2px;
 
         .navbar-brand {
             img {
