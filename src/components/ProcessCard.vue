355a8e2725c16355955cc5cba39543fe39883385
constantinandrei <andrei.todirascu@gmail.com>
2022-07-01 18:01:26 +0200

    create cards for process section

---
+++ src/components/ProcessCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col">
+        <div class="py-2">
+            <div class="square m-auto d-flex align-items-center justify-content-center" :style="{color: iconColor}">
+            <font-awesome-icon :icon="iconClass" />
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProcessCard',
+        props: {
+            iconClass: String,
+            iconColor: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .square {
+        width: 50%;
+        aspect-ratio: 1 / 1;
+        background-color: rgb(250, 247, 241);
+        border-radius: 50%;
+        position: relative;
+        z-index: 1;
+        font-size: 2rem;
+    }
+</style>
--- src/components/WorkingProcess.vue
+++ src/components/WorkingProcess.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="py-3">
         <div class="container">
-            <div class="px-5 position-relative">
+            <div class="w-75 m-auto position-relative">
                 <div class="row row-cols-4">
-                    <div class="col">
-                        maremma
-                    </div>
-                    <div class="col">
-                        maremma 
-                    </div>
-                    <div class="col">
-                        maremma
-                    </div>
-                    <div class="col">
-                        maremma
-                    </div>
+                    <ProcessCard
+                    :icon-class="'fa-solid fa-lightbulb'"
+                    :icon-color="'#BADAC9'" ></ProcessCard>
+                    <ProcessCard
+                    :icon-class="'fa-regular fa-comment'"
+                    :icon-color="'#F8D12D'" ></ProcessCard>
+                    <ProcessCard
+                    :icon-class="'fa-solid fa-cloud'"
+                    :icon-color="'#80465F'" ></ProcessCard>
+                    <ProcessCard
+                    :icon-class="'fa-solid fa-pencil'"
+                    :icon-color="'#EB6950'" ></ProcessCard>
                 </div>
-                <div class="position-absolute">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1095" height="126" viewBox="0 0 1095.3 126">
+                <div class="my-position-absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1095.3 126">
                         <path
                             d="M2.3 58s70 50.4 177 52c101.5 1.5 145.1-20.7 170-31.8 5.9-2.6 38.9-20.5 38.9-20.5 3.8-2.8 72.7-43.4 159.4-43.7s132.1 29.3 162.3 45.4c0 0 26.6 14.5 36.7 19.3 2.9 1.4 64.7 29.3 169.7 30.3s166-46.3 178-50"
                             fill="none" stroke-width="2" stroke="#f1cfc6" />
@@ -29,16 +29,19 @@
 </template>
 
 <script>
+import ProcessCard from './ProcessCard.vue';
     export default {
-        name: 'WorkingProcess'
-    }
+    name: "WorkingProcess",
+    components: { ProcessCard }
+}
 </script>
 
 <style lang="scss" scoped>
-    .position-absolute {
+    .my-position-absolute {
+        position: absolute;
         top: 0;
-        right: 0;
+        right: 8%;
         bottom: 0;
-        left: 0;
+        left: 8%;
     }
 </style>
