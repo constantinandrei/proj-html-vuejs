40dd20afdb7411d655c8d9f70779350bd2e59653
constantinandrei <andrei.todirascu@gmail.com>
2022-07-01 13:46:42 +0200

    jumbotron styled

---
+++ src/components/JumboImage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="h-100 position-relative">
+        <img class="woman" src="img/h-2-slider-img-15.png" alt="">
+        <img class="man" src="img/h-2-slider-img-16.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'JumboImage'
+    }
+</script>
+
+<style lang="scss" scoped>
+    img {
+        position: absolute;
+        height: 100%;
+    }
+
+    .woman {
+        top: 0;
+        left: 0;
+        bottom: 0;
+    }
+
+    .man {
+        top: 0;
+        bottom: 0;
+        right: 0;
+    }
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,41 @@
 <template>
-    <div>
-
+    <div class="section position-relative p-5">
+        <div class="container">
+            <div class="row row-cols-2">
+                <div class="col row align-items-center">
+                    <div class="col">
+                        <div class="p-5">
+                            <h1 class="letter-spacing">Devotion thad never <span>ends</span></h1>
+                            <p class="text-muted py-3">Nesque porro quisquantum est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+                            <div class="btn btn-outline">READ MORE</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <JumboImage></JumboImage>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import JumboImage from './JumboImage.vue';
     export default {
-        name: 'TheHeader'
-    }
+    name: "TheHeader",
+    components: { JumboImage }
+}
 </script>
+
+<style lang="scss" scoped>
+    h1 {
+        font-weight: 400;
+        font-size: 4rem;
+
+        span {
+            font-weight: bold;
+            font-style: italic;
+        }
+    }
+
+</style>
